56187e5302a3ea83002aa9297fb86b65afceab34
Modailun <modailun@gmail.com>
2025-09-29 17:44:29 +0200

    Fixed enemies movements

--- scripts/levels/formation_level_1.gd
+++ scripts/levels/formation_level_1.gd
@@ -1,18 +1,26 @@
 extends Node2D
 
-var direction : int = -1
-@export var speed : int = 32
-var sprite_size : int = 16
+var direction: int = -1
+var direction_y: int = 1
+@export var speed: int = 32
+@export var max_speed: int = 128
+var sprite_size: int = 16
+var distance_y: int = 4
 
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	# Récupère la taille actuelle de la fenêtre
+	var viewport_size = get_viewport_rect().size
 
-func _process(delta: float) -> void:
+	# Déplace l'objet horizontalement
 	position.x += speed * delta * direction
-	if position.x > 320 - 4 * sprite_size:
-		direction *= -1
-		position.y += 4
-	elif position.x < 4 * sprite_size:
-		direction *= -1
-		position.y += 4
 
+	# Inverse la direction horizontale si on atteint les bords
+	if position.x > viewport_size.x - 4 * sprite_size or position.x < 4 * sprite_size:
+		direction *= -1
+		position.y += direction_y * 4
+		speed = min(speed + 2, max_speed)
+
+    # Inverse la direction verticale si on atteint le haut ou le bas
+	if position.y > viewport_size.y - 4 * sprite_size or position.y < 3 * sprite_size:
+		direction_y *= -1
+		position.y += direction_y * 4
